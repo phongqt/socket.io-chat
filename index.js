d772d0733a8c5f825d0e69618349c8d5f2e20a64
phongqt1 <phongqt1703@gmail.com>
2017-06-23 17:39:15 +0700

    chat group, list of users

--- index.js
+++ index.js
@@ -1,49 +1,71 @@
-
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var clients = [], sequence = 1;;
+var clients = [],
+  sequence = 1,
+  sockets = [];
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+function getUserLoggin(data) {
+  let otherClients = [];
+  otherClients = clients.filter(function (item) {
+    if (item === data) {
+      return false;
+    }
+    return true;
+  }).map(function (item) {
+    return item;
+  })
+
+  return otherClients;
+}
+
+function handleListUserLogin() {
+  otherClients = [];
+  clients.forEach(function (item) {
+    otherClients = getUserLoggin(item);
+    let indx = clients.indexOf(item);
+    sockets[indx].emit('userlist', otherClients);
+  }, this);
+}
+
 io.on('connection', function (socket) {
   //new user
   socket.on('new user', function (data, callback) {
-    if (clients.indexOf(data) !== -1) { 
-      callback(false);
-    } else {
+    if (clients.indexOf(data) === -1) {
+      let indx = clients.push(data) - 1;
+      sockets[indx] = socket;
       socket.nickname = data;
-      clients.push(data);
-      console.log(clients);
-      io.emit('userlist', clients);
-      callback(true);
     }
+
+    handleListUserLogin();
+    // io.emit('userlist', otherClients);
+    callback(true);
   })
 
-  socket.emit('chat message', 'welcom to my app! ahihi');
+  // socket.emit('chat message', 'welcom to my app! ahihi');
   socket.on('chat message', function (msg) {
     // socket.broadcast.emit('hi');
-    io.emit('chat message', msg);
+    io.emit('chat message', {
+      from: socket.nickname,
+      message: msg
+    });
 
   });
+
   socket.on('disconnect', function () {
-    var index = clients.indexOf(socket);
+    var index = sockets.indexOf(socket);
     if (index != -1) {
-      clients.splice(index, 1);
-      console.info('Client gone (id=' + socket.id + ').');
+      delete sockets[index];
+      delete clients[index];
+      handleListUserLogin();
+      io.sockets.emit('user disconnect', index);
     }
   });
 });
-
-// setInterval(function() {
-//     var randomClient;
-//     if (clients.length > 0) {
-//         randomClient = Math.floor(Math.random() * clients.length);
-//         clients[randomClient].emit('chat message', sequence++);
-//     }
-// }, 1000);
 
 http.listen(3000, function () {
   console.log('listening on *:3000');
